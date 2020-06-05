0b13236a74a089b87faa0765b67ec17830f3852f
OksanaKuznetsova <Oksana_Kuznetsova@epam.com>
2020-06-05 20:41:45 +0300

    MyFirstProgram output message is updated

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,6 @@
 public class MyFirstProgram {
 
 	public static void main (String	[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hey Aleksei! I'm glad to start Java for Automated Testing course!");
 	}
 }
