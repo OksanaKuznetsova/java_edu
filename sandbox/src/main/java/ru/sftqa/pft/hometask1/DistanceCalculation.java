ddcd8ecd11683736e0a32a403f2ba20c69c4790e
OksanaKuznetsova <Oksana_Kuznetsova@epam.com>
2020-06-14 18:56:03 +0300

    Distance calculation moved as method to the Point class

--- sandbox/src/main/java/ru/sftqa/pft/hometask1/DistanceCalculation.java
+++ sandbox/src/main/java/ru/sftqa/pft/hometask1/DistanceCalculation.java
@@ -6,11 +6,8 @@ public class DistanceCalculation {
     Point p1 = new Point(-4, 4);
     Point p2 = new Point(0, 2.5);
 
-    System.out.println("Paccтояние между двумя точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") равно " + distance(p1, p2));
+    System.out.println("Paccтояние между двумя точками (" + p1.x + "," + p1.y + ") и (" + p2.x + "," + p2.y + ") равно " + p1.distance(p2));
   }
 
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2)));
-  }
 
 }
--- sandbox/src/main/java/ru/sftqa/pft/hometask1/Point.java
+++ sandbox/src/main/java/ru/sftqa/pft/hometask1/Point.java
@@ -8,4 +8,9 @@ public class Point {
     this.x = x;
     this.y = y;
   }
+
+  public double distance(Point p2) {
+    return Math.sqrt(Math.pow((this.x - p2.x), 2) + Math.pow((this.y - p2.y), 2));
+  }
+
 }
