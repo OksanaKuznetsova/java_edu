d2957a0b6924be8a23b4fb87fde81eb58ee8f48d
OksanaKuznetsova <Oksana_Kuznetsova@epam.com>
2020-06-14 17:48:47 +0300

    Added functions for area calculation

--- sandbox/src/main/java/ru/sftqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sftqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,26 @@ package ru.sftqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hey Aleksei! I'm glad to start Java for Automated Testing course!");
+        hello("world");
+        hello ("Aleksei");
+
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String who) {
+        System.out.println("Hey " + who + "! I'm glad to start Java for Automated Testing course!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area (double a, double b) {
+        return a * b;
     }
 }
