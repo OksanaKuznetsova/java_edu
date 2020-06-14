71437fe9b733a9086b34e28f9146489cc939ccf5
OksanaKuznetsova <Oksana_Kuznetsova@epam.com>
2020-06-14 16:45:58 +0300

    Code formatting is applied to the MyFirstProgram.java

--- sandbox/src/main/java/ru/sftqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sftqa/pft/sandbox/MyFirstProgram.java
@@ -2,7 +2,7 @@ package ru.sftqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main (String	[] args) {
-		System.out.println("Hey Aleksei! I'm glad to start Java for Automated Testing course!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hey Aleksei! I'm glad to start Java for Automated Testing course!");
+    }
 }
