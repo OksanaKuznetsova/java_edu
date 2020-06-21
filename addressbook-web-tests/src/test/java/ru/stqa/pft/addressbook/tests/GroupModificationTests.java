85b831fed2ed357a2081307da636e25e94fd201d
OksanaKuznetsova <Oksana_Kuznetsova@epam.com>
2020-06-21 17:36:13 +0300

    GroupModificationTests added

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("Renamed name 1", "Renamed header 1", "Renamed footer 1"));
+    app.getGroupHelper().submitGroupModification();
+    app.getNavigationHelper().returnToGroupPage();
+  }
+}
