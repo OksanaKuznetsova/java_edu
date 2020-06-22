23bcf77594dee194c2112de8f06a1f6040b308f4
OksanaKuznetsova <Oksana_Kuznetsova@epam.com>
2020-06-22 13:26:00 +0300

    Updated ContactModificationTests: using fillContactForm method for updating contact information

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -32,6 +32,10 @@ public class ContactHelper extends HelperBase {
     select(By.name("aday"), contactData.getAday());
     select(By.name("amonth"), contactData.getAmonth());
     type(By.name("ayear"), contactData.getAyear());
+
+  }
+
+  public void selectGroupForNewContact(ContactData contactData) {
     select(By.name("new_group"), contactData.getGroup());
   }
 
@@ -56,24 +60,4 @@ public class ContactHelper extends HelperBase {
     click(By.name("update"));
   }
 
-  public void updateContactDetails(ContactData contactData) {
-    type(By.name("firstname"), contactData.getFirstname());
-    type(By.name("middlename"), contactData.getMiddlename());
-    type(By.name("lastname"), contactData.getLastname());
-    type(By.name("nickname"), contactData.getNickname());
-    type(By.name("title"), contactData.getTitle());
-    type(By.name("company"), contactData.getCompany());
-    type(By.name("address"), contactData.getAddress());
-    type(By.name("home"), contactData.getHomephone());
-    type(By.name("mobile"), contactData.getMobilephone());
-    type(By.name("work"), contactData.getWorkphone());
-    type(By.name("email"), contactData.getEmail());
-    select(By.name("bday"), contactData.getBday());
-    select(By.name("bmonth"), contactData.getBmonth());
-    type(By.name("byear"), contactData.getByear());
-    select(By.name("aday"), contactData.getAday());
-    select(By.name("amonth"), contactData.getAmonth());
-    type(By.name("ayear"), contactData.getAyear());
-
-  }
 }
